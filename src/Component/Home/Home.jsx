43449467852fa45bbf2580e31d671b483e199a1f
Mohidul islam <sobujhasan1000@gamil.com>
2023-05-20 15:05:00 +0600

    How to buy section worke done

--- src/Component/Home/Home.jsx
+++ src/Component/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Bannar from './Bannar/Bannar';
 import Gallery from './Gallery/Gallery';
+import HowtoBuy from './HowtoBuy/HowtoBuy';
 
 const Home = () => {
     return (
         <div>
             <Bannar></Bannar>
+            <HowtoBuy></HowtoBuy>
            <Gallery></Gallery>
         </div>
     );
