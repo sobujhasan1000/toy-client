c8cfaa9cff20f3c16b3d496124e7a039ab6bc865
Mohidul islam <sobujhasan1000@gamil.com>
2023-05-20 19:06:07 +0600

    singel toy show deatiels work done

--- src/Component/AllToys/AllToy.jsx
+++ src/Component/AllToys/AllToy.jsx
@@ -8,18 +8,7 @@ const AllToy = () => {
     return (
         <>
             <h1 className='text-center my-4 text-xl'>ALL TOY</h1>
-            {/* <table className='w-3/4  mr-6'>
-            <thead>
-                        <tr>
-                            <th>seller Name</th>
-                            <th>toy name</th>
-                            <th>price</th>
-                            <th>sub catagory</th>
-                            <th>quantity</th>
-                            <th></th>
-                        </tr>
-                    </thead>
-            </table> */}
+           
             {
                 alltoys.map(toy=><Toys
                 key={toy._id}
--- src/Component/ToyView/Toyview.jsx
+++ src/Component/ToyView/Toyview.jsx
@@ -2,10 +2,29 @@ import React from 'react';
 import { useLoaderData, useParams } from 'react-router-dom';
 
 const Toyview = () => {
-    const toys=useLoaderData()
+    const toys = useLoaderData()
     return (
         <div>
-            <h1>singel toy {toys.name}</h1>
+            <div className="card bg-green-100 shadow-xl mb-4">
+                <figure className="px-10 pt-10">
+                    <img src={toys.toyPhoto} alt="Shoes" className="rounded-xl w-96" />
+                </figure>
+                <div className="card-body items-center text-center">
+                    <h2 className="card-title">Toy name: {toys.name}</h2>
+                   <div className='flex gap-4'>
+                   <p className='bg-slate-200 p-1 px-2 rounded-md'>seller Name:  {toys.selarName}</p>
+                    <p className='bg-slate-200 rounded-md px-2 '>seller Email: {toys.sellerEmail}</p>
+                   </div>
+                   <div className='flex gap-4'>
+                   <p className='bg-slate-200 p-1 rounded-md px-2 '>avaiable quantity:  {toys.totalQuantity}</p>
+                    <p className='bg-slate-200 rounded-md px-2 '>product description: {toys.description}</p>
+                   </div>
+                   <div className='flex gap-4'>
+                   <p className='bg-slate-200 p-1 rounded-md px-2 '>price:  {toys.price}</p>
+                    <p className='bg-slate-200 rounded-md px-2 '>Rating: {toys.rating}</p>
+                   </div>
+                </div>
+            </div>
         </div>
     );
 };
